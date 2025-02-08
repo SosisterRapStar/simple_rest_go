ef1fb623184ef4e069328f45eb680bad7aad32cb
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-02-08 16:12:21 +0300

    attempt2

--- internal/ports/note_port.go
+++
@@ -1,8 +0,0 @@
-package ports
-
-import "first-proj/application/domain"
-
-// internal port
-type NoteUseCase interface {
-	CreateNewNote(title string, description string, createdAt string) *domain.Note
-}
