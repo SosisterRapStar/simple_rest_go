c032a9f91d13f453f35fdd1730a75b55b7e135fd
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-02-15 20:46:01 +0300

    adde

--- postgres/postgres_service.go
+++ postgres/postgres_service.go
@@ -3,8 +3,10 @@ package postgres
 import (
 	"context"
 	"first-proj/domain"
+	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -13,9 +15,58 @@ type PostgresService struct {
 }
 
 func (pgs *PostgresService) CreateNote(ctx context.Context, note *domain.Note) (uuid.UUID, error) {
+	conn, err := pgs.db.Acquire(ctx)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("error occurred getting connection from pool: %w", err)
+
+	}
+	defer conn.Release()
+
+	tx, err := conn.BeginTx(ctx, pgx.TxOptions{IsoLevel: pgx.ReadCommitted})
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("Error occured during transaction opening: %w", err)
+	}
+
+	tx.Begin(ctx)
+	id, err := uuid.NewV7()
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("Error occured during uuid creating: %w", err)
+	}
+	note.Id = id
+	query := `INSERT INTO notes (id, title, content) VALUES ($1, $2, $3)`
+	_, err = tx.Exec(ctx, query, note.Id, note.Title, note.Content)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("error occurred during note insertion: %w", err)
+	}
+	defer tx.Rollback(ctx)
+
+	// Commit the transaction
+	if err := tx.Commit(ctx); err != nil {
+		return uuid.Nil, fmt.Errorf("error occurred during transaction commit: %w", err)
+	}
+
+	return note.Id, nil
+
 }
 
-func (pgs *PostgresService) FindNote(ctx context.Context, note *domain.Note) (uuid.UUID, error) {
+func (pgs *PostgresService) GetNote(ctx context.Context, id string) (*domain.Note, error) {
+	conn, err := pgs.db.Acquire(ctx)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("error occurred getting connection from pool: %w", err)
+
+	}
+	defer conn.Release()
+
+	tx, err := conn.BeginTx(ctx, pgx.TxOptions{IsoLevel: pgx.ReadCommitted})
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("Error occured during transaction opening: %w", err)
+	}
+
+	tx.Begin(ctx)
+	id, err := uuid.NewV7()
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("Error occured during uuid creating: %w", err)
+	}
 
 }
 
