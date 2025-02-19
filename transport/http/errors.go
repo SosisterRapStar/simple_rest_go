77f665e1e7111c4f9076a010f5668d63fc32a71b
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-02-19 20:27:45 +0300

    added some erros

--- transport/http/errors.go
+++ transport/http/errors.go
@@ -3,24 +3,26 @@ package server
 import (
 	"errors"
 	"first-proj/services"
+	"fmt"
+	"net/http"
 )
 
-
-
-
-
-
-
-type HttpApiError struct{
-	code int
-	details string
+type HttpApiError struct {
+	Status  int
+	Details string
 }
 
-
-
-
-func HandleServiceError(err error) HttpApiError{
-	apiError := HttpApiError{}
-	serviceErr : 
-	if errors.As(err)	
+func HandleServiceError(err error) HttpApiError {
+	var apiError HttpApiError
+	var serviceError services.ServiceError
+	if errors.As(err, &serviceError) {
+		appError := serviceError.GetAppError()
+		apiError.Details = appError.Error()
+		switch appError {
+		case services.ErrNoteNotFound:
+			apiError.Status = http.StatusNotFound
+			fmt.Println(serviceError.GetActualError()) // в будущем будет залогировано
+		}
+	}
+	return apiError
 }
