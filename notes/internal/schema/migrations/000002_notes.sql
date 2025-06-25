3b2fefe551d9bcaa4c99eba1b58fb591b7957e55
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-06-25 18:15:02 +0300

    refactor

--- notes/internal/schema/migrations/000002_notes.sql
+++ notes/internal/schema/migrations/000002_notes.sql
@@ -5,7 +5,9 @@ CREATE SCHEMA IF NOT EXISTS notes;
 CREATE TABLE IF NOT EXISTS notes.note (
     id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
     user_id UUID NOT NULL,
-    context TEXT,
+    name TEXT NOT NULL,
+    content TEXT,
+    expires_at TIMESTAMP, 
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP
